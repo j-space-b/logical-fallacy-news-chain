9b1ac00d3d1de8c1891e875df8fea87efd17cd42
Jon Bennion <120141355+j-space-b@users.noreply.github.com>
2023-09-15 08:20:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,37 @@
+# import packages
 import warnings
 warnings.filterwarnings('ignore')
 
-import streamlit as st, tiktoken
+import os
+import sys 
+import openai 
+sys.path.append('../..')
+from bs4 import BeautifulSoup
+import faiss
+import json
+from langchain import OpenAI, VectorDBQA
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import UnstructuredURLLoader
-from langchain.chains import LLMChain, SimpleSequentialChain
-from langchain.chains.summarize import load_summarize_chain
-from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import PyPDFLoader, JSONLoader, UnstructuredFileLoader, WebBaseLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains import LLMChain, SequentialChain, RetrievalQA
+from langchain.memory import VectorStoreRetrieverMemory
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain.utilities import GoogleSerperAPIWrapper
-from typing import Dict 
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.vectorstores import FAISS, Chroma
+from langchain.schema import Document
+from pathlib import Path
+from pprint import pprint
+import requests
+import streamlit as st, tiktoken 
+from unstructured.cleaners.core import clean_extra_whitespace
+from datetime import datetime, timedelta
+from langchain.docstore import InMemoryDocstore
+from langchain.retrievers import TimeWeightedVectorStoreRetriever
 
-
-# Streamlit input
-st.subheader('Topic to return news stories on')
-
-# Take values from UI 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", value="", type="password")
-    serper_api_key = st.text_input("Serper API Key", value="", type="password")
-    num_results = st.number_input("Number of Search Results", min_value=2, max_value=10)
-    st.caption("*Search: Summarizes any relevant news item from News APIs and describes any logical fallacies.*")
-search_query = st.text_input("Search Query", label_visibility="collapsed")
-col1, col2 = st.columns(2)
-
-rom typing import Dict
-
-from langchain.chains.fallacy_removal.models import LogicalFallacy
+# import 19 logical fallacies
+# source of extract: "https://arxiv.org/pdf/2212.07425.pdf
+# will be used to evaluate results when using this as a datasource rather than the pdf 
 
 fallacies = {
     "Logical Fallacy Name": ["Adhominem","Adpopulum","Appeal to Emotion","Fallacy of Extension",
@@ -53,48 +59,173 @@ fallacies = {
                    "erroneously inferring that something is true of the whole based on the fact that it is true of some part or parts",
                    "erroneously inferring that something is true of the parts based on the fact that it is true of the whole"]
 }
+json_str = json.dumps(fallacies, indent=4)
 
-# If 'Search' button is clicked
-if col2.button("Search"):
+
+with open("fallacies.json", "w") as json_file:
+    json_file.write(json_str)
+
+
+# Load dict as JSON - could not quickly find an obvious method to load dict as same format that loader.load expects
+
+loader = JSONLoader(
+    file_path='./fallacies.json',
+    jq_schema='.',
+    text_content=False)
+
+data = loader.load()
+
+# Split dictionary using same methodology as the pdf - similar amount of docs from splits as the PDF
+
+chunk_size=50
+chunk_overlap=10
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=chunk_size,
+    chunk_overlap=chunk_overlap,
+    length_function=len
+)
+docs2 = text_splitter.split_documents(data)
+len(docs2)
+
+# Setup faiss retreiver and save for later reference
+
+db2 = FAISS.from_documents(docs2, embeddings)
+retriever2 = db2.as_retriever(search_kwargs={"k":2})
+model2 = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever2)
+save_directory2 = "FallacyDict"
+db2.save_local(save_directory2)
+
+# Vectorize for embeddings if/where needed
+chunk_size=50
+chunk_overlap=10
+
+text_splitter_news = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size, chunk_overlap=chunk_overlap, separators=[" ", ",", "\n"]
+    )
+
+news = text_splitter_news.split_documents(datanews)
+
+# Define model using OpenAI 
+embeddings_model3 = OpenAIEmbeddings()
+embedding_size = 1536
+index = faiss.IndexFlatL2(embedding_size)
+vectorstore3 = FAISS(embeddings_model3.embed_query, index, InMemoryDocstore({}), {})
+retriever3 = TimeWeightedVectorStoreRetriever(vectorstore=vectorstore3, decay_rate=.999999, k=2)
+memory = VectorStoreRetrieverMemory(retriever=retriever3) #if/where needed
+
+
+ # Chain 1: Summarize any possible logical fallacies in the text of the article
+
+template1 = """You are a communications expert who speaks nothing but truth and logic and is \
+extremely clear for a wide audience.  Given a full news article, your job is to summarize \
+it accurately and with brevity in one sentence, then find any logical fallacies that \
+may exist and return examples in no more than 1 sentence per logical fallacy found.  \
+If more than one logical fallacies are found, return the top 2, in order of logical strength, \
+unless no logical fallacies are found, in which then state no strong logical fallacies are clearly evident. \
+Article: {datanews} \
+Communications expert: 
+Summary:"""
+prompt_template1 = PromptTemplate(input_variables=["datanews"], template=template1)
+chain1 = LLMChain(llm=llm, prompt=prompt_template1, output_key='summary')
+
+ # Chain 2: Analyze the implications of any logical fallacies in the article in relation to the article summary
+
+template2 = """You are an engaging professor who only speaks with truth and sound logic \
+while clearly conveying a point in as few words as possible.  Given the text of a news article as defined, if the title reads 'Access Denied' then state no access \
+to the article is available. If the title does not read 'Access Denied', then create two outputs: Analysis and Counterfactual. \
+For the Analysis output, it will be three parts.  The first part is labeled 'Summary' and returns {summary}.  The second part is labeled 'Analysis' and is two sentences.  First, you need \
+to return the top ranked logical fallacy in the article, among any logical fallacies that may exist, ranked by order of logical strength, \
+described with brevity in one sentence and confirming this logical fallacy is correct by extracting factual evidence \
+from the article text, then finally referencing the extracted fact in the description. Be sure to state the strongest logical fallacy might not be strong, \
+so is only to consider. Create the second sentence of the Analysis output by stating why this fallacy might be dangerous to the public or \
+especially misleading in the context of the news article, with respect to how other readers could react. \
+The third part is labeled 'Theoretical Counterfactual', explain any counterfactuals to the summary of the article ({summary}) that could hypothetically be true, \
+based on logic and the limited facts presented in the article.  If more than one counterfactuals exist, only return  \
+the top ranked counterfactual, ranked in order of logical strength and feasibility, described with brevity in 1 sentence. \ 
+Professor: \
+Summary: {summary}\
+Analysis: \ 
+Theoretical Counterfactual: """
+prompt_template2 = PromptTemplate(input_variables=["summary"], template=template2)
+chain2 = LLMChain(llm=llm, prompt=prompt_template2, output_key='analysis')
+
+
+
+# Replicating above functionality but using Streamlit for input
+st.subheader('Enter search terms:')
+
+with st.sidebar:
+    openai.api_key = st.text_input("OpenAI API Key", value="", type="password")
+    serper_api_key = st.text_input("Serper API Key", value="", type="password")
+    num_results = st.number_input("Number of Search Results", min_value=3, max_value=5)
+    st.caption("*Search: Uses Serper & OpenAI APIs, summarizes each search result.*")
+    st.caption("*URL Lookup: Analyzes a specific URL*")
+search_query = st.text_input("Search Query", label_visibility="collapsed")
+col1, col2 = st.columns(2)
+
+# If the 'Search' button is clicked
+if col1.button("Search"):
     # Validate inputs
-    if not openai_api_key.strip() or not serper_api_key.strip() or not search_query.strip():
-        st.error(f"Please provide the missing fields.")
+    if not api_key.strip() or not serper_api_key.strip() or not search_query.strip():
+        st.error(f"Please provide the API keys or the missing search terms.")
     else:
         try:
-            with st.spinner("Loading..."):
-                # Orders news items by relevancy from previous week
+            with st.spinner("Analyzing articles..."):
+                # Show the top X relevant news articles from the previous week using Google Serper API
                 search = GoogleSerperAPIWrapper(type="news", tbs="qdr:w1", serper_api_key=serper_api_key)
                 result_dict = search.results(search_query)
 
                 if not result_dict['news']:
                     st.error(f"No search results for: {search_query}.")
                 else:
-                    # Load URLs
                     for i, item in zip(range(num_results), result_dict['news']):
-                        loader = UnstructuredURLLoader(urls=[item['link']])
-                        data = loader.load()
+                        url = item.get('link','N/A') 
+                        if url == 'N/A':
+                            continue  
+                        loader = WebBaseLoader(url) # bs4 
+                        try:
+                            datanews = loader.load()
+                            overall_chain1 = SequentialChain(chains=[chain1, chain2],
+                                input_variables=["datanews"],
+                                output_variables=["analysis"],
+                                verbose=True)
+                            first = (overall_chain1({"datanews":datanews}))
+                            st.success(f"Logical Fallacy Critique: {item['analysis']}\n\nLink: {item['link']}")
+                        except Exception as e:
+                            print(f"Error fetching {item['link']}, exception: {e}")
+        except Exception as e:
+            st.exception(f"Exception: {e}")
 
-                        # Chain 1: Summarize Any Possible Logical Fallacies
-                        llm = ChatOpenAI(temperature=0, model='gpt-3.5-turbo', openai_api_key=openai_api_key)
-                        prompt1 = ChatPromptTemplate.from_template(
-                          "Think carefully about how the {data} may be representing any of the logical fallacy name descriptions contained in {FALLACIES}, and remember each instance as a logical fallacy in {data}"
-                           "\n\nIf one ore more logical fallacies exist in {data}: Return a short sentence to summarize the full article then summarize each portion of the article that contains a logical fallacy, with the logical fallacy evident in the summary of the article portion."
-                           "\n\nIf zero logical follacies are found in {data}: Return a short sentence to summarize the full article then return the following sentence as the last sentence 'No Fallacies Found'
-                          "
-                        )
-                        chain1 = LLMChain(llm=llm, prompt=prompt1)
+# If 'Search & Summarize' button is clicked
+if col2.button("URL Lookup"):
+    # Validate inputs
+    if not api_key.strip() or not serper_api_key.strip() or not search_query.strip():
+        st.error(f"Please provide the API keys or missing URL in the search term window.")
+    else:
+        try:
+            with st.spinner("Analyzing articles..."):
+                # Show the top X relevant news articles from the URL entered - lookup since URLs change
+                search = GoogleSerperAPIWrapper(type="news", tbs="qdr:w1", serper_api_key=serper_api_key)
+                result_dict = search.results(search_query)
 
-                        # Chain 2: Name Each Logical Fallacy and Describe Harm
-                        prompt2 = ChatPromptTemplate.from_template(
-                          "If the summary reads 'No Fallacies Found', then return 'No Fallacies Found'."
-                          "\n\nIf the summary does not read 'No Fallacies Found' then name each logical fallacy found according to its name in {fallacies}, summarize a portion of the article where the fallacy is found in 10 words, then explain in an additional 10 words why the logical fallacy may harm society as it exists in news.  Do this until all logical fallacies found in the summary are listed."
-                        )
-                        chain2 = LLMChain(llm=llm, prompt=prompt2)
-
-                        overall_chain = SimpleSequentialChain(chains=[chain1,chain2], verbose=True)
-
-                        summary_enriched = overall_chain.run(data)
-
-                        st.success(f"Title: {item['title']}\n\nLink: {item['link']}\n\nSummary: {summary_enriched}")
+                if not result_dict['news']:
+                    st.error(f"No search results for: {search_query}.")
+                else:
+                    for i, item in zip(range(num_results), result_dict['news']):
+                        url = item.get('link','N/A') 
+                        if url == 'N/A':
+                            continue  
+                        loader = WebBaseLoader(url) # bs4 
+                        try:
+                            datanews = loader.load()
+                            overall_chain1 = SequentialChain(chains=[chain1, chain2],
+                                input_variables=["datanews"],
+                                output_variables=["analysis"],
+                                verbose=True)
+                            first = (overall_chain1({"datanews":datanews}))
+                            st.success(f"Logical Fallacy Critique: {item['analysis']}\n\nLink: {item['link']}")
+                        except Exception as e:
+                            print(f"Error fetching {item['link']}, exception: {e}")
         except Exception as e:
             st.exception(f"Exception: {e}")
